48c229b1fa172b0e31cb591d304c7b9350217428
José Ricardo Tovarg III <jose.2710.ricardo@gmail.com>
2024-04-29 17:53:22 -0400

    Marcadores

---
+++ form.php
@@ -0,0 +1,224 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+
+    <title>Biker Rock Amazonas MC</title>
+
+
+    <meta name="description" content="Nice and creative portfolio template. Fully responsive with cool animations">
+    <meta name="keywords" content="agency, bootstrap 4, creative, flexbox, jquery, minimalist, motion design, onepage, photography, portfolio, product, responsive, studio, video">
+
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1, user-scalable=0">
+
+    <link rel="icon" type="image/png" sizes="16x16" href="resources/img/logo.png">
+
+    <link rel="stylesheet" href="resources/fonts/opensans/stylesheet.css">
+    <link rel="stylesheet" href="resources/fonts/ionicons/ionicons.min.css">
+    <link rel="stylesheet" href="resources/fonts/fontawesome/font-awesome.min.css">
+
+    <link rel="stylesheet" href="resources/css/pageloader.css">
+
+    <link rel="stylesheet" href="resources/css/bootstrap.min.css">
+    <link rel="stylesheet" href="resources/js/vendor/swiper.min.css">
+    <link rel="stylesheet" href="resources/js/vendor/jquery.fullpage.min.css">
+    <link rel="stylesheet" href="resources/js/vegas/vegas.min.css">
+
+    <link rel="stylesheet" href="resources/css/main.css">
+    <link rel="stylesheet" href="resources/css/style-gradient.css">
+</head>
+
+
+</style>
+
+<body id="menu" class="body-page">
+
+
+    <div class="page-loader" id="page-loader">
+        <div>
+            <div class="icon ion-spin"></div>
+        </div>
+    </div>
+
+    <header class="page-header navbar page-header-alpha scrolled-white menu-right topmenu-right">
+        <a class="navbar-brand" href="index.html#home">
+            <span class="logo">
+                <img class="light-logo" src="resources/img/logo.png" alt="Logo">
+            </span>
+        </a>
+
+    </header>
+
+
+    <div class="page-cover">
+
+        <div class="cover-bg bg-img" data-image-src="resources/img/fondo.jpg"></div>
+
+        <div class="cover-bg-mask bg-color" data-bgcolor="rgba(2, 3, 10, 0.7)"></div>
+    </div>
+
+
+    <main class="page-main page-fullpage main-anim" id="mainpage">
+
+
+
+
+        <style>
+            .mcontrol {
+                background-color: #ffffff38;
+                color: white;
+            }
+        </style>
+        <div class="section section-register fp-auto-height-responsive ">
+
+            <div style="width: 70%;">
+
+
+                <div class=" anim ">
+                    <div class="row ">
+                        <div class="col-12 col-md-12 col-lg-12">
+
+                            <div class="form-container-transparent form-container-white">
+                                <div class="form-desc">
+                                    <h3 class="mb-0 display-title anim-2 nowrap">Formulario de admisión</h3>
+                                    <p class="invite anim-3 text-muted"><small>Rellene el formulario, una vez verifiquemos sus datos, le contactaremos.</small></p>
+                                </div>
+                                <div class="form-input  anim-4 mt-3 ">
+
+                                    <div class="row">
+
+
+                                        <div class="col-lg-6">
+
+                                            <div class="form-group form-success-gone">
+                                                <label for="reg-cedula">Cédula</label>
+                                                <input id="reg-cedula" class="form-control mcontrol" />
+                                            </div>
+
+
+                                            <div class="form-group form-success-gone">
+                                                <label for="reg-nombre">Nombre</label>
+                                                <input id="reg-nombre" class="form-control mcontrol" />
+                                            </div>
+
+                                            <div class="form-group form-success-gone">
+                                                <label for="reg-fn">Fecha de nacimiento</label>
+                                                <input id="reg-fn" class="form-control mcontrol" type="date" />
+                                            </div>
+
+                                            <div class="form-group form-success-gone">
+                                                <label for="reg-cedula">Teléfono</label>
+                                                <input id="reg-cedula" class="form-control mcontrol" />
+                                            </div>
+
+
+                                            <div class="form-group form-success-gone">
+                                                <label for="reg-cedula">Correo</label>
+                                                <input id="reg-cedula" class="form-control mcontrol" />
+                                            </div>
+                                            <div class="form-group form-success-gone">
+                                                <label for="reg-cedula">Lugar de residencia</label>
+                                                <input id="reg-cedula" class="form-control mcontrol" />
+                                            </div>
+                                        </div>
+
+                                        <div class="col-lg-6">
+                                            <div class="form-group form-success-gone">
+                                                <label for="reg-cedula">Foto de la cedula</label>
+                                                <input id="reg-cedula" type="file" class="form-control mcontrol" />
+                                            </div>
+
+                                            <div class="form-group form-success-gone">
+                                                <label for="reg-cedula">Foto del carnet de circulación o titulo</label>
+                                                <input id="reg-cedula" type="file" class="form-control mcontrol" />
+                                            </div>
+
+                                            <div class="form-group form-success-gone">
+                                                <label for="reg-cedula">Enfermedades que sufre (opcional)</label>
+                                                <input id="reg-cedula" class="form-control mcontrol" />
+                                            </div>
+
+                                            <div class="form-group form-success-gone">
+                                                <label for="reg-cedula">Años de experiencia manejando</label>
+                                                <select id="reg-cedula" class="form-control mcontrol">
+                                                    <option value="">Seleccione</option>
+                                                    <option value="<1">Menos de un año</option>
+                                                    <option value="1-3">Entre uno y tres</option>
+                                                    <option value="3-5">Entre tres y cinco</option>
+                                                    <option value="+5">Más cinco</option>
+                                                </select>
+                                            </div>
+
+
+
+                                            <div class="form-group form-success-gone">
+                                                <label for="reg-cedula">Experiencia manejando por caminos irregulares</label>
+                                                <select id="reg-cedula" class="form-control mcontrol">
+                                                    <option value="">Seleccione</option>
+                                                    <option value="0">Ninguna</option>
+                                                    <option value="<1">Menos de un año</option>
+                                                    <option value="1-3">Entre uno y tres</option>
+                                                    <option value="3-5">Entre tres y cinco</option>
+                                                    <option value="+5">Más cinco</option>
+                                                </select>
+                                            </div>
+
+
+                                            <div class="form-group form-success-gone">
+                                                <label for="reg-cedula">Antecedentes penales</label>
+                                                <select id="reg-cedula" class="form-control mcontrol">
+                                                    <option value="">Seleccione</option>
+                                                    <option value="Si">Si</option>
+                                                    <option value="No">No</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group mb-0 text-center">
+                                        <div>
+                                            <p class="email-ok  form-text-feedback form-success-visible" style="display: none;" id="informacion"></p>
+                                        </div>
+                                        <button id="submit" class="btn btn-outline-white form-success-gone">Iniciar
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+
+        </div>
+
+
+    </main>
+
+
+    <footer id="site-footer" class="page-footer">
+
+        <div class="footer-right">
+            <p class="note">Creado por:
+                <span style="font-weight: bold;"> Tercero </span>
+                Cartografía y tecnología
+            </p>
+
+        </div>
+    </footer>
+
+
+
+    <script src="resources/js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="resources/js/vendor/all.js"></script>
+    <script src="resources/js/form_script.js"></script>
+
+    <script src="resources/js/main.js"></script>
+
+
+</body>
+
+
+
+</html>
--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 
 <!doctype html>
 <html class="no-js" lang="en">
-
 <head>
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
 
@@ -44,9 +43,8 @@
 
     <header class="page-header navbar page-header-alpha scrolled-white menu-right topmenu-right">
 
-        <button class="navbar-toggler site-menu-icon" id="navMenuIcon">
-
-            <img src="resources/img/alerta.png" alt="Alerta">
+        <button class="navbar-toggler site-menu-icon">
+        <a href="login.php" class="btn btn-outline-white btn-sm">Iniciar sesión</a>
 
         </button>
 
@@ -59,51 +57,29 @@
 
         <div class="all-menu-wrapper" id="navbarMenu">
 
-            <nav class="navbar-mainmenu" style="display: flex;">
-                <div style="display: grid; place-items: center; margin: auto">
-                    <h1>Recomendaciones:</h1>
-                    <p class="mt-3" style="max-width: 60%; text-align: center;">
-
-                        Uniforme base incluyendo cubre boca de tela (
-                        Chaqueta negra. Casco integral o semi integral. Botas o zapatos acordes a las rodadas. Guantes).
-                        <br>
-                        <br>
-                        Papeles personales y de la motocicleta al día
-                        <br>
-                        <br>
-                        Saber de memoria el lenguaje de señas de Motociclistas.
-                        <br>
-                        <br>
-
-                        Herramientas básicas para sus motos
-                        <br>
-                        <br>
-
-                        Equipos o Herramientas de repuestos
-                        <br>
-                        <br>
-
-                        Botiquín básico de primeros auxilios
-                        <br>
-                        <br>
-
-                        Respetar la ruta establecida y demás compañeros del club
-                        <br>
-                        <br>
-                        Hidratación particular
-                        <br>
-                        <br>
-                        La colaboración correspondiente a lo que se vaya a hacer o preparar durante el recorrido.
-
-
-
-
-
-
-                    </p>
-                </div>
+            <nav class="navbar-sidebar ">
+                <ul class="navbar-nav" id="qmenu">
+                    <li class="nav-item" data-menuanchor="home">
+                        <a href="index.php#home">
+                            <i class="icon ion-ios-home"></i>
+                            <span class="txt">Inicio</span>
+                        </a>
+                    </li>
+                    <li class="nav-item" data-menuanchor="about">
+                        <a href="index.php#about">
+                            <i class="icon ion-star"></i>
+                            <span class="txt">Sobre el club</span>
+                        </a>
+                    </li>
+                    <li class="nav-item" data-menuanchor="team">
+                        <a href="index.php#team">
+                            <i class="icon ion-ios-people"></i>
+                            <span class="txt">Jefatura</span>
+                        </a>
+                    </li>
+                   
+                </ul>
             </nav>
-
 
 
         </div>
@@ -121,10 +97,7 @@
 
     <main class="page-main page-fullpage main-anim" id="mainpage">
 
-
-
-
-        <div class="section section-register fp-auto-height-responsive " data-section="login">
+        <div class="section section-register fp-auto-height-responsive " data-section="home">
 
             <div class="section-wrapper center-vh dir-col">
 
@@ -142,22 +115,11 @@
                                     <p class="invite anim-3 opacity-75">Ingresa tu número de cédula y fecha de nacimiento.</p>
                                 </div>
                                 <div class="form-input  anim-4">
-                                    <div class="form-group form-success-gone">
-                                        <label for="reg-cedula">Cédula</label>
-                                        <input id="reg-cedula" class="form-control-line form-control-white" type="number" required placeholder="00 000 000" />
-                                    </div>
-                                    <div class="form-group form-success-gone">
-                                        <label for="reg-fecha">Fecha de nacimiento</label>
-                                        <input id="reg-fecha"  class="form-control-line form-control-white" type="date" required />
-                                    </div>
 
 
                                     <div class="form-group mb-0 text-center">
-                                        <div>
-                                            <p class="email-ok  form-text-feedback form-success-visible" style="display: none;" id="informacion"></p>
-                                        </div>
-                                        <button id="submit" class="btn btn-outline-white form-success-gone">Iniciar
-                                        </button>
+                                        <a href="form.php" class="btn btn-outline-white form-success-gone">Llenar formulario de admisión
+                                        </a>
                                     </div>
                                 </div>
                             </div>
@@ -170,9 +132,159 @@
 
         </div>
 
+        <div class="section section-description fp-auto-height-responsive about-section" data-section="about">
+
+            <div class="section-wrapper center-vh dir-col anim">
+
+                <div class="section-title text-center">
+                    <h5 class="title-bg">About</h5>
+                    <h2 class="display-4 display-title anim-2 mb-32">About Us</h2>
+                </div>
+
+                <div class="section-content reduced center-vh dir-col">
+
+                    <div class="col-12 col-sm-12 center-vh">
+                        <div class="section-content anim translateUp">
+                            <div class="images text-center">
+                                <div class="img-frame-legend-alpha">
+                                    <div>
+                                        <img class="img shadow" src="resources/img/familia.jpeg" alt="Image">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-8 col-sm-8 about-description shadow">
+                        <div class="section-content anim">
+
+                            <h4 class="text-center">Our Mission</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris aliquet
+                                malesuada feugiat. Curabitur fermentum bibendum nulla, non dictum
+                                ipsum tincidunt non. Quisque convallis pharetra tempor. Donec id
+                                pretium leo. Pellentesque luctus massa non elit viverra pellentesque.
+                                Cras vitae neque molestie, rhoncus ipsum sit amet, lobortis dui.
+                                Fusce in urna sem.</p>
+                        </div>
+                    </div>
+                </div>
+
+
+                <footer class="section-footer scrolldown">
+                    <a class="down">
+                        <span class="icon"></span>
+                        <span class="txt">Services</span>
+                    </a>
+                </footer>
+            </div>
+
+        </div>
+
+
+
+
+        
+        <div class="section section-description fp-auto-height-responsive " data-section="team">
+
+            <div class="section-wrapper center-vh dir-col anim">
+
+                <div class="section-title text-center">
+                    <h5 class="title-bg">Team</h5>
+                    <h2 class="display-4 display-title anim-2 mb-32">Team</h2>
+                </div>
+
+                <div class="section-content anim text-center">
+
+
+                    <div class="item row justify-content-between">
+
+                        <div class="col-12 col-sm-6 col-md-4 center-vh">
+                            <div class="section-content anim translateUp">
+                                <div class="images text-center">
+                                    <div class="img-avatar-alpha">
+                                        <div class="img-1 shadow">
+                                            <a href="item.html">
+                                                <img class="img" src="resources/img/team/2.png" alt="Image">
+                                            </a>
+                                        </div>
+                                        <div class="legend text-center pos-abs">
+                                            <div class="pull-left text-left">
+                                                <h5>O'dosha</h5>
+                                                <p class="small">Presidente</p>
+                                            </div>
+                                            <div class="icons pull-right text-right">
+                                                <a class="icon-btn" href="index.html#">
+                                                    <i class="icon ion-social-facebook"></i>
+                                                </a>
+                                            </div>
+                                            <div class="clearfix"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-12 col-sm-6 col-md-4 center-vh">
+                            <div class="section-content anim">
+                                <div class="images text-center">
+                                    <div class="img-avatar-alpha">
+                                        <div class="img-1 shadow">
+                                            <a href="item.html">
+                                                <img class="img" src="resources/img/team/1.PNG" alt="Image">
+                                            </a>
+                                        </div>
+                                        <div class="legend text-center pos-abs">
+                                            <div class="pull-left text-left">
+                                                <h5>Tercero</h5>
+                                                <p class="small">Jefe de operaciones</p>
+                                            </div>
+                                            <div class="icons pull-right text-right">
+                                                <a class="icon-btn" href="index.html#">
+                                                    <i class="icon ion-social-instagram-outline"></i>
+                                                </a>
+                                            </div>
+                                            <div class="clearfix"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-12 col-sm-6 col-md-4 center-vh">
+                            <div class="section-content anim translateDown">
+                                <div class="images text-center">
+                                    <div class="img-avatar-alpha">
+                                        <div class="img-1 shadow">
+                                            <a href="item.html">
+                                                <img class="img" src="resources/img/team/3.png" alt="Image">
+                                            </a>
+                                        </div>
+                                        <div class="legend text-center pos-abs">
+                                            <div class="pull-left text-left">
+                                                <h5>Aquiles</h5>
+                                                <p class="small">Jefe de seguridad</p>
+                                            </div>
+                                            <div class="icons pull-right text-right">
+                                                <a class="icon-btn" href="index.html#">
+                                                    <i class="icon fa fa-twitter"></i>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+        </div>
 
     </main>
 
+
+
+    
 
     <footer id="site-footer" class="page-footer">
 
